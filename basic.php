4c23b5f589299c0a369d3e7771180e394602e9a5
HAB-Coder <habibamursaleen20@gmail.com>
2025-06-03 02:49:21 +0500

    add basic.php file

---
+++ basic.php
@@ -0,0 +1,20 @@
+<?php
+
+//  echo  is used to print the output on the screen.
+echo "Welcome to backened development";
+echo '<br>';
+
+//  $  is used to declare a variable in PHP.
+$num =  10;
+$a = 'Coding zone';
+$b = 'PHP';
+
+
+//  .  is used to concatenate.
+echo $b.' is used for backened'.'<br>'.$a;
+echo '<br>';
+
+// var_dump() is used to print the data type of the variable it also include spaces as well.
+var_dump($a); // output : string(11) "Coding zone"
+
+?>
