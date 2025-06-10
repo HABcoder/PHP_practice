9373fa804dabdefba6e5c023f93afc2832b50ace
HAB-Coder <habibamursaleen20@gmail.com>
2025-06-11 01:19:23 +0500

    add arthematics operators

--- arthematics_operators.php
+++ arthematics_operators.php
@@ -1,6 +1,29 @@
 <?php
+
 $a =60;
 $b = 30;
 $sum = $a + $b;
-echo $sum;
+echo '60 + 30 = '.$sum."<br>";
+echo "<br>";
+
+$c = 2;
+$d = 6;
+echo '2 - 6 = '.$c - $d."<br>";
+echo "<br>";
+
+
+$multiply = $a * $b;
+echo '60 * 30 = '.$multiply."<br>";
+echo "<br>";
+
+$e = 3;
+$f = 9; 
+$divide = $f / $e;
+echo '3 / 9 = '.$divide."<br>";
+echo "<br>";
+
+$tricky = $sum / $d + $divide - 8;
+echo '60 + 30 / 6 + 3 / 9 - 8 = '.$tricky;
+
+
 ?>
