8b60c8365d2d95436dcce58b90e284ed4db2d372
HAB-Coder <habibamursaleen20@gmail.com>
2025-06-14 19:11:12 +0500

    add new array

--- array.php
+++ array.php
@@ -5,5 +5,9 @@ $fruits = array('Mango','Grapes','Orange');
 echo $fruits[1];
 echo '<br>'
 
-
+$colors = ["color1" => 'Pink', "color2" => 'Blue', "color3" => 'Green'];
+echo $colors["color2"];
+foreach($colors as $k => $v) {
+    echo $v;
+}
 ?>
