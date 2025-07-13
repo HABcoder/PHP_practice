65bd0d263fd4827863d31e942f36a8fb430d1490
HAB-Coder <habibamursaleen20@gmail.com>
2025-07-13 20:41:51 +0500

    add new function

--- str_function.php
+++ str_function.php
@@ -9,4 +9,14 @@ echo "<br>";
 $str2 = "Computer";
 $chars = str_shuffle($str2);
 echo $chars;  // Outputs: retpmouc
+
+
+echo "<br>";
+# string ky center sy koi value get karni ho
+$str3 = "John Doe,, Welcome again";
+echo substr($str3, 0, 5);  // Outputs: John
+echo "<br>";
+echo substr($str3,0 , 1); // Outputs: J
+echo "<br>";
+echo substr($str2,4,2);
 ?>
